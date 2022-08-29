22680453739e7df13292256798427258f1cb733c
Ethiem <ethiemalex03@gmail.com>
2022-08-29 18:24:09 -0500

    route admin

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import Paciente from './components/paciente/Index'
 import Dashboard_em from './components/empresa/dashboard/Dashboard';
 import Dashboard from './components/paciente/dashboard/Dashboard';
+import Dashboard_admin from '../src/components/administrador/Dashboard';
 import Empresa from './components/empresa/Index';
 import Login from './Login';
 import './App.css';
@@ -16,6 +17,7 @@ function App() {
           <Route path='/empresa' element={<Empresa/>} ></Route>
           <Route path='/empresa/dashboard' element={<Dashboard_em/>} ></Route>
           <Route path='/login' element={<Login/>}></Route>
+          <Route path='/administrador/dashboard' element={<Dashboard_admin />} ></Route>
           <Route path='*' element={<Error />} ></Route> 
         </Routes>
       </BrowserRouter>

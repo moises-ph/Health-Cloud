1657677dd540d033a820e49782ddb853fc9f9820
moisesph <mois.mp8@gmail.com>
2022-09-01 21:52:31 +0000

    Header

--- routers/Login.js
+++ routers/Login.js
@@ -1,32 +1,34 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcrypt');
-const { UserSchema } = require('../models/users');
-const jwt = require('jsonwebtoken');
-const joi = require('joi');
+const bcrypt = require("bcrypt");
+const { UserSchema } = require("../models/users");
+const jwt = require("jsonwebtoken");
+const joi = require("joi");
 
 const schemaLogin = joi.object({
-    num_documento: joi.number().required(),
-    password: joi.string().min(6).required()
+  num_documento: joi.number().required(),
+  password: joi.string().min(6).required(),
 });
 
-router.post('/',async (req, res)=>{
-    // Validate Data
-    const { error } = schemaLogin.validate(req.body);
-    if(error) return res.status(400).json({msg: error.details[0].message});
+router.post("/", async (req, res) => {
+  // Validate Data
+  const { error } = schemaLogin.validate(req.body);
+  if (error) return res.status(400).json({ msg: error.details[0].message });
 
-    const { num_documento, password } = req.body;
+  const { num_documento, password } = req.body;
 
-    const user = await UserSchema.findOne({num_documento: num_documento});
-    if(!user) return res.status(400).json({msg: 'User not found'});
+  const user = await UserSchema.findOne({ num_documento: num_documento });
+  if (!user) return res.status(400).json({ msg: "User not found" });
 
-    const validPassword = await bcrypt.compare(password, user.password);
-    if(!validPassword) return res.status(400).json({msg: 'Invalid password'});
+  const validPassword = await bcrypt.compare(password, user.password);
+  if (!validPassword) return res.status(400).json({ msg: "Invalid password" });
 
-    const token = jwt.sign({_id: user._id, numUser : user.num_documento}, process.env.TOKEN_SECRET);
+  const token = jwt.sign(
+    { _id: user._id, numUser: user.num_documento },
+    process.env.TOKEN_SECRET
+  );
 
-    res.header('auth-token', token).json({msg: 'User logged in'});
-
+  res.setHeader("auth-token", token).json({ msg: "User logged in" });
 });
 
 module.exports = router;

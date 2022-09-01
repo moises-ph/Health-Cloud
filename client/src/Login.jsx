faa1240e9af34ce26ce054653665ca8314f51ab5
Mjhon03 <Mjhon1841@gmail.com>
2022-08-31 21:33:14 -0500

    Terminar logica login front

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useRef } from "react";
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 
 function Login() {
+
+  const num_documento = useRef();
+  const password = useRef();
+
+  const logVerify = (e)=>{
+    e.preventDefault();
+    let num = num_documento.current.value;
+    let pass = password.current.value;
+
+    axios.post('/login', {num_documento: num, password: pass})
+      .then((res) => console.log(res)).catch(err => console.log(err));
+  }; 
+
   return (
     <>
-      <form>
+      <form onSubmit={logVerify}>
         <input
           type="text"
           className="outline-none bg-slate-500 mx-3 rounded-md text-slate-50"
           min="3"
+          max="10"
+          ref={num_documento}
+          required
         />
         <input
           type="password"
           className="outline-none bg-slate-500 rounded-md text-slate-50"
-          min="8"
+          min="6"
+          ref={password}
+          required
         />
         <button>realizar</button>
       </form>
